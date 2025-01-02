bad7181dba02db059e2880791801f101e75ca273
Max Wofford <max@maxwofford.com>
2025-01-02 13:21:37 -0500

    Remove unused import/export

--- src/app/harbor/signpost/help.ts
+++ src/app/harbor/signpost/help.ts
@@ -5,13 +5,6 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
   process.env.BASE_ID!,
 )
 
-//TODO: Delete this file it's weird
-import { getWakaSessions } from '@/app/utils/waka'
-
-export async function wakaSessions() {
-  return await getWakaSessions()
-}
-
 export async function getStickyUrls(): string[] {
   const allStickies = (await base('sticky_holidays').select({}).all()).sort(
     (a, b) => a.fields.id - b.fields.id,
