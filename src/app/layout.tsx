74632763cffaf174162ac4eebd7f7d28078e772f
Max Wofford <max@maxwofford.com>
2025-01-12 13:16:49 -0500

    Remove oversized favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
           href="/favicons/favicon-96x96.png"
           sizes="96x96"
         />
-        <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
+        <link rel="icon" href="/favicons/favicon.ico" />
         <link rel="shortcut icon" href="/favicons/favicon.ico" />
         <link
           rel="apple-touch-icon"
