16f313a40f68fab59e09040af75537112730e578
Max Wofford <max@maxwofford.com>
2025-01-22 12:48:31 -0500

    Add daily shirt generation job trigger

--- src/app/api/cron/every-day/route.ts
+++ src/app/api/cron/every-day/route.ts
@@ -1,10 +1,48 @@
 export const dynamic = 'force-dynamic'
 export const fetchCache = 'force-no-store'
 
-import createBackgroundJob from '../create-background-job'
+import 'server-only'
+import Airtable from 'airtable'
+
+async function triggerShirtJob() {
+  Airtable.configure({
+    apiKey: process.env.AIRTABLE_API_KEY,
+    endpointUrl: process.env.AIRTABLE_ENDPOINT_URL,
+  })
+
+  const base = Airtable.base(process.env.BASE_ID)
+  console.log('Getting people to regenerate')
+  const peopleToRegenerate = await base('people')
+    .select({
+      filterByFormula: `
+    AND(
+      NOT({action_generate_shirt_design} = TRUE()),
+      NOT({ysws_submission} = BLANK())
+    )`,
+      fields: [],
+    })
+    .all()
+
+  const peopleIds = peopleToRegenerate.map((person) => person.id)
+  console.log('People to regenerate:', peopleIds.length)
+  const chunkSize = 10
+  for (let i = 0; i < peopleIds.length; i += chunkSize) {
+    console.log(`Processing chunk ${i} to ${i + chunkSize}`)
+    const chunk = peopleIds.slice(i, i + chunkSize)
+    await base('people').update(
+      chunk.map((id) => ({
+        id,
+        fields: {
+          action_generate_shirt_design: true,
+        },
+      })),
+    )
+  }
+}
 
 async function processDailyJobs() {
   console.log('Processing daily jobs')
+  await triggerShirtJob()
 }
 
 export async function GET() {
